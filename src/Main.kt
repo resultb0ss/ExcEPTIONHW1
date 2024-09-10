ec67d34d59c3def007076a6b9da2c46e5bc09bd4
Admin <reteamboss@gmail.com>
2024-09-10 20:13:36 +0300

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,43 @@
 fun main() {
-    println("Hello World!")
+    var array: Array<String?> = arrayOfNulls<String>(10)
+    createList(array)
+
+
+}
+
+fun greeting(array: Array<String>){
+    array.forEach { println("$it Добрый день") }
+}
+
+fun createList(list: Array<String?>): Array<String?> {
+
+    try {
+        println("Введите имя и Фамилию пользователя")
+        var inputName = readln()
+        var check = MyException()
+        check.checkNull(inputName)
+        println("Введите позицию в каталоге")
+        var inputPosition = readln()
+        list[inputPosition.toInt()] = inputName
+    } catch (e: Exception) {
+        println(e.message)
+    } catch (e: MyException) {
+        println(e.message)
+    }
+    return list
+}
+
+class MyException : Exception() {
+
+    override val message: String?
+        get() = "Недопустимое значение: Пустая строка"
+
+    fun checkNull(str: String): String {
+        if (str == ""){
+            throw Exception(
+                "Недопустимое значение: Пустая строка"
+            )
+        }
+        return str
+    }
 }
